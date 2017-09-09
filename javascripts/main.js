c1bb8f8894e16384f233b7cd14464c9400ba2471
Dre mf Randaci <drerandaci@gmail.com>
2017-09-09 14:04:54 -0500

    added buttons and filters

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,3 +7,24 @@ document.getElementById('fancyPetStore').addEventListener('click', (e) => {
 		CatStore.adoptCat(catIndex);
 	};
 });
+
+document.getElementById('callan').addEventListener('click', (e) => {
+	console.log('callan event:', e)
+	CatStore.getCatsByOwner(2);
+});
+
+document.getElementById('zoe').addEventListener('click', (e) => {
+	console.log('zoe event', e)
+	CatStore.getCatsByOwner(1);
+});
+document.getElementById('lauren').addEventListener('click', (e) => {
+	CatStore.getCatsByOwner(3);
+});
+document.getElementById('allCats').addEventListener('click', (e) => {
+	let cats = CatStore.getCats();
+	let owners = CatStore.getOwners();
+	CatStore.combineArrays(cats, owners);
+});
+document.getElementById('none').addEventListener('click', (e) => {
+	CatStore.getCatsByOwner(0);
+});
